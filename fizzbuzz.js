930756418ed025120b67edd3c8af0a501f92e4a3
martincavaliers <martincoutts5@gmail.com>
2018-06-18 18:43:58 +0100

    Logging user input to console and appending it to a UL on dcoument

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,8 @@
 const input = document.getElementById('countInput');
 const submit = document.getElementById('submitBtn');
 
+let valueArray = new Array();
+
 
 submit.addEventListener("click", (e) => {
 
@@ -10,14 +12,26 @@ submit.addEventListener("click", (e) => {
 
 	for (let i = 1; i <= count; i++){
 	if(i%3 === 0 && i%5 === 0){
+		valueArray.push("FIZZBUZZ!!!");
 		console.log("FIZZBUZZ!!!")
 	}else if(i%3 === 0){
+		valueArray.push("Fizz");
 		console.log("Fizz")
 	}else if(i%5 === 0){
+		valueArray.push("Buzz");
 		console.log("Buzz")
 	}else{
+		valueArray.push(i);
 		console.log(i)
 	}
 }
+
+	for (let i = 0; i < valueArray.length; i++){
+		var node = document.createElement("LI");
+		var textnode = document.createTextNode(valueArray[i]);
+		node.appendChild(textnode);
+		document.getElementById('arrayList').appendChild(node);	
+	}
+	
 });
 
